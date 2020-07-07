2e88ab28d2c5e849e29c6939de15a162533a9bfb
nakulrandad <nakulrandad@gmail.com>
2020-07-07 22:32:00 +0530

    window added

--- game.py
+++ game.py
@@ -16,17 +16,20 @@ MAIN_BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "bg_im
 
 def game_begin():
     run = True
+    title_font = pygame.font.SysFont("comicsans", 60)
     while run:
         WIN.blit(MAIN_BG, (0,0))
-        # title_label = title_font.render("Press spacebar to start", 1, (255,255,255))
-        # WIN.blit(title_label, (int((WIDTH - title_label.get_width())/2), int(7/15 * HEIGHT)))
-        # pygame.display.update()
-        # keys = pygame.key.get_pressed()
-        # for event in pygame.event.get():
-        #     if event.type == pygame.QUIT:
-        #         run == False
-        #         sys.exit()
-        #     if keys[pygame.K_SPACE]:
-        #         pass
+        title_label = title_font.render("Press spacebar to start", 1, (0,0,255))
+        WIN.blit(title_label, (int((WIDTH - title_label.get_width())/2), int(7/15 * HEIGHT)))
+        pygame.display.update()
+        keys = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run == False
+                sys.exit()
+            if keys[pygame.K_SPACE]:
+                pass
+    # sys.exit()
 
-    sys.exit()
+if __name__ == "__main__":
+    game_begin()
